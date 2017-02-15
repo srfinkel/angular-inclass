4934f047b5b3297b0591b29bbb059ce854b46089
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-02-15 10:02:38 -0700

    Added enable debug mode

--- app/app.ts
+++ app/app.ts
@@ -2,6 +2,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 import { enableProdMode } from '@angular/core';
 
-enableProdMode();
+// enableProdMode();
+// uncomment on launch. helps with debugging tools
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
