c206d6e9797af0bb4ddd87a134370af979f6f4a5
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-02-15 11:36:57 -0700

    Added comments to Route, index.php and changed webpack to 1.14.0

--- public_html/templates/angular-inclass-app.php
+++ public_html/templates/angular-inclass-app.php
@@ -1,3 +1,4 @@
 <main class="container">
+	<!-- This is by the Angular Route Module -->
 	<router-outlet></router-outlet>
 </main>
--- webpack/index.php
+++ webpack/index.php
@@ -9,6 +9,8 @@ if(session_status() !== PHP_SESSION_ACTIVE) {
 		<meta charset="UTF-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!-- The <base> sets the routes up for success; it makes all URLs relative to this URL. Needed for all the templates to relate to this fixed point -->
+		<!-- PHP_SELF no matter what your deployment directories are, it will pull from those directories. Alt would be to static;y direct it to one dir-->
 		<base href="<?php echo dirname($_SERVER["PHP_SELF"]) . "/"; ?>" />
 
 		<title>Angular Inclass</title>
